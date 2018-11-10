6e2e7ebf9119c708dbdacad1bc28dfc3733cd50a
lfmnovaes <lfmnovaes@gmail.com>
2018-11-10 20:21:34 -0200

    routes working and error handling

---
+++ api/routes/planets.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: 'GET request to /planets'
+    });
+});
+
+router.post('/', (req, res, next) => {
+    res.status(201).json({
+        message: 'POST request to /planets'
+    });
+});
+
+router.get('/:planetId', (req, res, next) => {
+    const id = req.params.planetId;
+    if (id === 'special') {
+        res.status(200).json({
+            message: 'Special ID',
+            id: id
+        });
+    } else {
+        res.status(200).json({
+            message: 'Normal ID'
+        });
+    }
+});
+
+router.patch('/:planetId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Updated planet!'
+    });
+});
+
+router.delete('/:planetId', (req, res, next) => {
+    res.status(200).json({
+        message: 'Deleted planet!'
+    });
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+const planetRoutes = require('./api/routes/planets');
+
+app.use(morgan('dev'));
+
+// Routes
+app.use('/planets', planetRoutes);
 
 app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'It works!'
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
     });
 });
 
